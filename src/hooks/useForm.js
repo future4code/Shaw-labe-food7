444fc2b253d62aebedbd2e23d1c1d871d5f1f848
Renan Martinez <remam_martinez89@hotmail.com>
2022-05-16 15:13:42 -0300

    criado useForm e constante url

---
+++ src/hooks/useForm.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const useForm = (initialState) => {
+  const [form, setForm] = React.useState(initialState)
+
+  const onChange = (event) => {
+    const { name, value } = event.target
+    setForm({...form,  [name]: value });
+  };
+
+  const clean = () => {
+    setForm(initialState)
+  }
+
+  return { form, onChange, clean };
+}
